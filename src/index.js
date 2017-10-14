b9af31e842c65d37c66ab90cfc5fb33c9ec2dd97
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-14 16:32:16 -0700

    Added all the necessary work to get firebase working.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('This is hermes.');
+import * as functions from 'firebase-functions';
+import admin from 'firebase-admin';
+
+admin.initializeApp(functions.config().firebase);
+
+exports.hello = functions.https.onRequest((req, res) => res.send(200));
