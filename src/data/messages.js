203fbcf0fd852f12898154408a8f3822db8526a6
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-14 19:33:05 -0700

    Setup visual placeholders for all of the message types.

--- src/data/messages.js
+++ src/data/messages.js
@@ -9,49 +9,49 @@ export default {
     start: {
         type: 'text',
         payload: {
-            text: 'Welcome good sir, thank you for starting.',
+            text: 'Hermes welcomes you!\n\nSelect "Join a Game" to begin.',
         },
     },
     queue: {
         type: 'text',
         payload: {
-            text: 'We\'ve added you to the queue.',
+            text: 'You have been added to the Queue. When it has 5 people we will start the game and let you know.',
         },
     },
     join: {
         type: 'text',
         payload: {
-            text: 'We set you up in a game.',
+            text: 'A game has started Jill!\nYour adversaries are:\n\n - Jack\n - Jenn\n - Jim\n - Jess\n\n<Goal Card>',
         },
     },
     hand: {
         type: 'text',
         payload: {
-            text: 'Here\'s your hand.',
+            text: '<Hand Carousel>',
         },
     },
     pick: {
         type: 'text',
         payload: {
-            text: 'Good choice.',
+            text: 'You picked:\n\n<Picked Card>\n\nWe\'ll let you know when your adversaries have selected their candidates.',
         },
     },
     candidates: {
         type: 'text',
         payload: {
-            text: 'Here are the candidates.',
+            text: '<Voting List>',
         },
     },
     vote: {
         type: 'text',
         payload: {
-            text: 'We hope you win.',
+            text: 'Here are your votes:\n\n<Vote List>\n\nNow we wait for the votes and we\'ll let you know when they are in.',
         },
     },
     winner: {
         type: 'text',
         payload: {
-            text: 'You are the winner.',
+            text: 'And the winner is…\n\n<Winner Card>\n\nAnd now the next goal...\n\n<Next Goal Card>',
         },
     },
     unknown: {
