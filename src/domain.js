f71231636eef1cf2f8f07d0f1c333945adea5445
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-16 20:08:20 -0700

    Refactored the domain class a bit.

--- src/domain.js
+++ src/domain.js
@@ -2,23 +2,19 @@ import transmute from 'transmutation';
 
 export default ({ db }) => ({
     login: transmute()
-        .extend('player', ({ lead }) => db.get(`players/${lead.platform}/${lead.id}`))
+        .extend('player', ({ lead }) =>
+            db.get(`players/${lead.platform}/${lead.id}`))
         .switch('player', {
             undefined: transmute()
+                // TODO: Create a uuid for the player
                 .do(({ lead }) => db.set(`players/${lead.platform}/${lead.id}`, lead))
                 .extend('messages', [
-                    {
-                        type: 'welcome-message',
-                    },
-                    {
-                        type: 'instructions-message',
-                    },
+                    { type: 'welcome-message' },
+                    { type: 'instructions-message' },
                 ]),
             _default: transmute()
                 .extend('messages', [
-                    {
-                        type: 'welcome-back-message',
-                    },
+                    { type: 'welcome-back-message' },
                 ]),
         }),
 });
