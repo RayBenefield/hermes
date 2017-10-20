ce6c141dc611f34dd560dfa74d2f6de7c8543fc9
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-20 06:26:49 -0700

    Changed the sample messages to use transmutation to not lose any data from the beginning of the stream.

--- src/data/messages.js
+++ src/data/messages.js
@@ -1,112 +1,121 @@
 /* eslint-disable max-lines */
+import transmute from 'transmutation';
+
 export default {
-    queue: () => ({ messages: [
-        {
-            type: 'queue-joined-message',
-            payload: {
-                numberOfPeople: 3,
-            },
-        },
-    ] }),
-    join: () => ({ messages: [
-        {
-            type: 'game-started-message',
-            payload: {
-                player: 'Jill',
-                players: [
-                    'Jack',
-                    'Jenn',
-                    'Jim',
-                    'Jess',
-                ],
-            },
-        },
-        {
-            type: 'new-goal-message',
-            payload: {
-                card: {
-                    contents: '________, the latest Facebook craze.',
+    queue: transmute()
+        .extend('messages', [
+            {
+                type: 'queue-joined-message',
+                payload: {
+                    numberOfPeople: 3,
                 },
             },
-        },
-    ] }),
-    hand: () => ({ messages: [
-        {
-            type: 'show-hand-message',
-            payload: {
-                cards: [
-                    { contents: 'Being a motherfucking sorcerer.' },
-                    { contents: 'Winking at old people.' },
-                    { contents: 'THE KOOL-AID MAN.' },
-                    { contents: 'Hurricane Katrina.' },
-                    { contents: 'Powerful thighs.' },
-                    { contents: 'Vigorous jazz hands.' },
-                    { contents: 'BEES?' },
-                    { contents: 'Morgan Freeman\'s voice.' },
-                    { contents: 'Racism.' },
-                    { contents: 'Daddy issues.' },
-                ],
-            },
-        },
-    ] }),
-    pick: () => ({ messages: [
-        {
-            type: 'card-selected-message',
-            payload: {
-                contents: 'Being a motherfucking sorcerer.',
-            },
-        },
-    ] }),
-    candidates: () => ({ messages: [
-        {
-            type: 'show-ranked-list',
-            payload: {
-                ranked: [
-                    { contents: 'Vigorous jazz hands' },
-                    { contents: 'Centaur Porn' },
-                ],
-                unranked: [
-                    { contents: 'Your Face' },
-                    { contents: 'Barack Obama' },
-                ],
-            },
-        },
-    ] }),
-    vote: () => ({ messages: [
-        {
-            type: 'show-votes-message',
-            payload: {
-                ranked: [
-                    { contents: 'Vigorous jazz hands' },
-                    { contents: 'Centaur Porn' },
-                    { contents: 'Your Face' },
-                    { contents: 'Barack Obama' },
-                ],
-            },
-        },
-        {
-            type: 'wait-for-votes-message',
-            payload: {
-                numberOfPeople: 2,
-            },
-        },
-    ] }),
-    winner: () => ({ messages: [
-        {
-            type: 'show-winner-message',
-            payload: {
-                goal: { contents: '________, the latest Facebook craze.' },
-                winner: { contents: 'Being a motherfucking sorcerer.' },
-                player: { firstName: 'Jill' },
-            },
-        },
-        {
-            type: 'show-next-goal-message',
-            payload: {
-                card: {
-                    contents: 'How am I maintaining my relationship status?',
+        ]),
+    join: transmute()
+        .extend('messages', [
+            {
+                type: 'game-started-message',
+                payload: {
+                    player: 'Jill',
+                    players: [
+                        'Jack',
+                        'Jenn',
+                        'Jim',
+                        'Jess',
+                    ],
                 },
             },
-        },
-    ] }),
+            {
+                type: 'new-goal-message',
+                payload: {
+                    card: {
+                        contents: '________, the latest Facebook craze.',
+                    },
+                },
+            },
+        ]),
+    hand: transmute()
+        .extend('messages', [
+            {
+                type: 'show-hand-message',
+                payload: {
+                    cards: [
+                        { contents: 'Being a motherfucking sorcerer.' },
+                        { contents: 'Winking at old people.' },
+                        { contents: 'THE KOOL-AID MAN.' },
+                        { contents: 'Hurricane Katrina.' },
+                        { contents: 'Powerful thighs.' },
+                        { contents: 'Vigorous jazz hands.' },
+                        { contents: 'BEES?' },
+                        { contents: 'Morgan Freeman\'s voice.' },
+                        { contents: 'Racism.' },
+                        { contents: 'Daddy issues.' },
+                    ],
+                },
+            },
+        ]),
+    pick: transmute()
+        .extend('messages', [
+            {
+                type: 'card-selected-message',
+                payload: {
+                    contents: 'Being a motherfucking sorcerer.',
+                },
+            },
+        ]),
+    candidates: transmute()
+        .extend('messages', [
+            {
+                type: 'show-ranked-list',
+                payload: {
+                    ranked: [
+                        { contents: 'Vigorous jazz hands' },
+                        { contents: 'Centaur Porn' },
+                    ],
+                    unranked: [
+                        { contents: 'Your Face' },
+                        { contents: 'Barack Obama' },
+                    ],
+                },
+            },
+        ]),
+    vote: transmute()
+        .extend('messages', [
+            {
+                type: 'show-votes-message',
+                payload: {
+                    ranked: [
+                        { contents: 'Vigorous jazz hands' },
+                        { contents: 'Centaur Porn' },
+                        { contents: 'Your Face' },
+                        { contents: 'Barack Obama' },
+                    ],
+                },
+            },
+            {
+                type: 'wait-for-votes-message',
+                payload: {
+                    numberOfPeople: 2,
+                },
+            },
+        ]),
+    winner: transmute()
+        .extend('messages', [
+            {
+                type: 'show-winner-message',
+                payload: {
+                    goal: { contents: '________, the latest Facebook craze.' },
+                    winner: { contents: 'Being a motherfucking sorcerer.' },
+                    player: { firstName: 'Jill' },
+                },
+            },
+            {
+                type: 'show-next-goal-message',
+                payload: {
+                    card: {
+                        contents: 'How am I maintaining my relationship status?',
+                    },
+                },
+            },
+        ]),
 };
