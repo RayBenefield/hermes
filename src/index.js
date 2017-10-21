6d76d4dd25d7539e75378aae4458c2eba030dddb
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-21 08:07:56 -0700

    Refactored out the channel router.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import _ from 'lodash';
-import express from 'express';
 import admin from 'firebase-admin';
-import transmute from 'transmutation';
 import * as functions from 'firebase-functions';
 import configureFacebook from './lib/facebook';
 import * as domain from './domain/services'; // eslint-disable-line import/no-unresolved, import/extensions
 import configureContext from './domain/context';
 import configureDb from './lib/db';
 import messages from './data/messages';
-import { logAll } from './lib/utils';
+import configureChannelRouter from './routes/channels';
 
 const config = functions.config();
 admin.initializeApp(config.firebase);
@@ -20,36 +18,8 @@ const getContext = context.get;
 const saveContext = context.save;
 const enterDomain = _.extend(messages, domain);
 
-const router = express();
-router.post('/facebook', (req, res) => transmute({ raw: req.body })
-    .do(() => res.sendStatus(200))
-    .extend('lead', fb.extractLead)
-    .extend(fb.extractActionWithPayload)
-    .switch('action', {
-        login: transmute().extend('lead', fb.enrichLead),
-    })
-    .switch('action', getContext)
-    .extend('messages', transmute()
-        .switch('action', enterDomain))
-    .do(stream => Promise.all(
-        stream.messages.map(msg =>
-            transmute(msg).switch('type', saveContext))
-    ))
-    .extend('facebookMessages', fb.transform)
-    // eslint-disable-next-line no-console
-    .do(logAll)
-    .then(({ lead: { id }, facebookMessages }) =>
-        fb.sendMessages(id, facebookMessages))
-);
-router.get('/facebook', (req, res) => {
-    try { fb.verifyToken(req.query); } // eslint-disable-line brace-style
-    catch (err) {
-        console.error(err); // eslint-disable-line no-console
-        return res.sendStatus(403);
-    }
-    return res.status(200).send(req.query['hub.challenge']);
-});
-
+const channelRouter =
+    configureChannelRouter({ fb, getContext, enterDomain, saveContext });
 
 // Handle incoming messages
-exports.channels = functions.https.onRequest(router);
+exports.channels = functions.https.onRequest(channelRouter);
---
+++ src/routes/channels.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import transmute from 'transmutation';
+import { logAll } from '../lib/utils';
+
+export default ({ fb, getContext, enterDomain, saveContext }) => {
+    const router = express();
+    router.post('/facebook', (req, res) => transmute({ raw: req.body })
+        .do(() => res.sendStatus(200))
+        .extend('lead', fb.extractLead)
+        .extend(fb.extractActionWithPayload)
+        .switch('action', {
+            login: transmute().extend('lead', fb.enrichLead),
+        })
+        .switch('action', getContext)
+        .extend('messages', transmute()
+            .switch('action', enterDomain))
+        .do(stream => Promise.all(
+            stream.messages.map(msg =>
+                transmute(msg).switch('type', saveContext))
+        ))
+        .extend('facebookMessages', fb.transform)
+        // eslint-disable-next-line no-console
+        .do(logAll)
+        .then(({ lead: { id }, facebookMessages }) =>
+            fb.sendMessages(id, facebookMessages))
+    );
+    router.get('/facebook', (req, res) => {
+        try { fb.verifyToken(req.query); } // eslint-disable-line brace-style
+        catch (err) {
+            console.error(err); // eslint-disable-line no-console
+            return res.sendStatus(403);
+        }
+        return res.status(200).send(req.query['hub.challenge']);
+    });
+    return router;
+};
