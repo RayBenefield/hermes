d6645e2088e28ccfdbc353f046eef708e1fce217
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-21 07:14:33 -0700

    Removed a line in the database lib using a default value in the call.

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,8 +2,7 @@ export default db => ({
     get: (key, defaultValue) => db.ref(key).once('value')
         .then(snapshot => snapshot.val() || defaultValue),
     set: (key, value = true) => db.ref(key).set(value),
-    delete: (keys) => {
-        const updates = {};
+    delete: (keys, updates = {}) => {
         keys.forEach(key => updates[key] = null);
         return db.ref().update(updates);
     },
