8cd238c87a6639226857c9e5cb2dbf931bfab716
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-21 11:49:49 -0700

    Added additional side effects to game including creating a hand.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines */
 import _ from 'lodash';
 import admin from 'firebase-admin';
 import transmute from 'transmutation';
@@ -25,8 +26,22 @@ const channelRouter =
 // Handle incoming messages
 exports.channels = functions.https.onRequest(channelRouter);
 
+const hand = [
+    { contents: 'Being a motherfucking sorcerer.' },
+    { contents: 'Winking at old people.' },
+    { contents: 'THE KOOL-AID MAN.' },
+    { contents: 'Hurricane Katrina.' },
+    { contents: 'Powerful thighs.' },
+    { contents: 'Vigorous jazz hands.' },
+    { contents: 'BEES?' },
+    { contents: 'Morgan Freeman\'s voice.' },
+    { contents: 'Racism.' },
+    { contents: 'Daddy issues.' },
+];
+
 exports.gameStarted = functions.database.ref('/games/{id}')
     .onCreate(event => transmute({
+        game: { id: event.params.id },
         action: 'start',
         payload: event.data.val(),
     })
@@ -38,9 +53,13 @@ exports.gameStarted = functions.database.ref('/games/{id}')
         ])
         .extend('leads', ({ payload: { players, notified_players: notified } }) =>
             _.values(players).filter(({ id }) => !(id in notified)))
-        .then(({ leads, messages: rawMessages }) => Promise.all(
+        .do(({ leads, messages: rawMessages }) => Promise.all(
             leads.map((lead) => {
                 const facebookMessages = fb.transform({ lead, messages: rawMessages });
                 return fb.sendMessages(lead.id, facebookMessages);
             })))
+        .do(({ game, payload: { players } }) =>
+            db.set(`games/${game.id}/notified_players`, players))
+        .do(({ game, payload: { players } }) => Promise.all(_.values(players)
+            .map(p => db.set(`hands/${game.id}/${p.id}`, hand))))
     );
