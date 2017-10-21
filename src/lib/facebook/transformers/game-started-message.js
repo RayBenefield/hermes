ddc384e26bff84957f1aef9b95c93b31ba1bbe95
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-21 07:45:37 -0700

    Added names to the game started message.

--- src/lib/facebook/transformers/game-started-message.js
+++ src/lib/facebook/transformers/game-started-message.js
@@ -3,6 +3,6 @@ import text from './text';
 export default ({ players }) => text({
     text: `A game has started!
 Your adversaries are:
-${Object.values(players).map(p => `\n - ${p.name}`)}
+${Object.values(players).map(p => `\n - ${p.first_name || 'Anonymous'}`).join('')}
 `,
 });
