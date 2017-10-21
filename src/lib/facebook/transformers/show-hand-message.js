89b6ece9f78101c07edacc09041c331686ae8933
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-21 16:52:57 -0700

    Added pick payload.

--- src/lib/facebook/transformers/show-hand-message.js
+++ src/lib/facebook/transformers/show-hand-message.js
@@ -1,14 +1,14 @@
 import carousel from './carousel';
 
 export default ({ cards }) => carousel(
-    cards.map(({ contents }) => ({
+    cards.map(({ id, contents }) => ({
         title: contents,
         image_url: 'http://homepages.neiu.edu/~whuang2/cs300/images/white.png',
         buttons: [
             {
                 type: 'postback',
                 title: 'Pick',
-                payload: 'action: pick',
+                payload: `action: pick\npayload: ${id}`,
             },
         ],
     }))
