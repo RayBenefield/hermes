64d3e866bf774079754af598f10f231426948a02
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-21 06:57:29 -0700

    Removed the need for lodash in the database library.

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,10 +1,10 @@
-import _ from 'lodash';
-
 export default db => ({
     get: (key, defaultValue) => db.ref(key).once('value')
         .then(snapshot => snapshot.val() || defaultValue),
     set: (key, value = true) => db.ref(key).set(value),
-    delete: keys => db.ref().update(_.fromPairs(
-        keys.map(k => ([k, null]))
-    )),
+    delete: (keys) => {
+        const updates = {};
+        keys.map(key => updates[key] = null); // eslint-disable-line no-return-assign
+        return db.ref().update(updates);
+    },
 });
