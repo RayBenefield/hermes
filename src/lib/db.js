8f3a55d620355407f7a1e6fe0efe44afdce1961e
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-21 06:48:29 -0700

    Removed an unused method.

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,9 +4,6 @@ export default db => ({
     get: (key, defaultValue) => db.ref(key).once('value')
         .then(snapshot => snapshot.val() || defaultValue),
     set: (key, value = true) => db.ref(key).set(value),
-    getByChild: (key, property, propValue) =>
-        db.ref(key).orderByChild(property).equalTo(propValue).once('value')
-            .then(snapshot => _.values(snapshot.val())),
     delete: keys => db.ref().update(_.fromPairs(
         keys.map(k => ([k, null]))
     )),
