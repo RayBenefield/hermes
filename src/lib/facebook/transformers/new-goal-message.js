865549e0ab22446dc6d704c9c0c850c65f83a60d
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-21 16:43:57 -0700

    Added a show hand button to new goals.

--- src/lib/facebook/transformers/new-goal-message.js
+++ src/lib/facebook/transformers/new-goal-message.js
@@ -4,6 +4,13 @@ export default ({ card: { contents } }) => list([
     {
         title: contents,
         imageUrl: 'http://b.basemaps.cartocdn.com/dark_all/11/604/771.png',
+        buttons: [
+            {
+                title: 'Show Hand',
+                type: 'postback',
+                payload: 'action: hand',
+            },
+        ],
     },
     {
         title: '🏆',
