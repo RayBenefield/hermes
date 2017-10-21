724f24c2d6d0b7f4b7284e418d0ff248fdf715fc
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-21 11:12:29 -0700

    Modified the populate queue script to read from the database and default add test accounts.

--- dev/populate-queue.js
+++ dev/populate-queue.js
@@ -1,13 +1,36 @@
+import transmute from 'transmutation';
+import { prompt } from 'inquirer';
 import admin from 'firebase-admin';
 import configureDb from '../src/lib/db';
 import privateKey from './private-key.json';
-import testAccounts from './data/test-accounts';
 
 admin.initializeApp({
     credential: admin.credential.cert(privateKey),
     databaseURL: 'https://hermes-dev-1fc82.firebaseio.com',
 });
 const db = configureDb(admin.database());
+const testAccounts = ['1969918279691174', '2062932073720600', '1654450144625528', '1572553382787981'];
 
-// eslint-disable-next-line no-console
-db.set('queue', testAccounts).then(() => process.exit(0));
+transmute({
+    template: {
+        type: 'checkbox',
+        name: 'queue',
+        message: 'Which players should we add to the queue?',
+    },
+})
+    .extend('players', db.get('players/facebook'))
+    .extend('choices', ({ players }) => Object.values(players)
+        .map(player => ({
+            name: `${player.first_name} ${player.last_name}`,
+            value: player.id,
+            short: player.first_name,
+            checked: testAccounts.includes(player.id),
+        }))
+    )
+    .extend('prompts', ({ template, choices }) => [{ ...template, choices }])
+    .extend('answers', ({ prompts }) => prompt(prompts))
+    .extend('playersToAdd', ({ players, answers: { queue } }) => queue.map(p => players[p]))
+    .extend('queue', ({ playersToAdd }) => playersToAdd
+        .reduce((queue, player) => Object.assign(queue, { [player.id]: player }), {}))
+    .do(({ queue }) => db.set('queue', queue))
+    .then(() => process.exit(0));
