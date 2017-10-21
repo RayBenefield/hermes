9ae0d954acb55340e16b9c0bf4368ef3eab49e0c
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-21 09:22:58 -0700

    Added everything needed to message all other players when a game is started.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import _ from 'lodash';
 import admin from 'firebase-admin';
+import transmute from 'transmutation';
 import * as functions from 'firebase-functions';
 import configureFacebook from './lib/facebook';
 import * as domain from './domain/services'; // eslint-disable-line import/no-unresolved, import/extensions
@@ -22,3 +24,23 @@ const channelRouter =
 
 // Handle incoming messages
 exports.channels = functions.https.onRequest(channelRouter);
+
+exports.gameStarted = functions.database.ref('/games/{id}')
+    .onCreate(event => transmute({
+        action: 'start',
+        payload: event.data.val(),
+    })
+        .extend('messages', ({ payload }) => [
+            {
+                type: 'game-started-message',
+                payload,
+            },
+        ])
+        .extend('leads', ({ payload: { players, accepted_players: notified } }) =>
+            _.values(players).filter(({ id }) => !(id in notified)))
+        .then(({ leads, messages: rawMessages }) => Promise.all(
+            leads.map((lead) => {
+                const facebookMessages = fb.transform({ lead, messages: rawMessages });
+                return fb.sendMessages(lead.id, facebookMessages);
+            })))
+    );
--- src/lib/facebook/transformers/game-started-message.js
+++ src/lib/facebook/transformers/game-started-message.js
@@ -1,10 +1,10 @@
+import _ from 'lodash';
 import text from './text';
 
 export default ({ lead, players }) => text({
     text: `A game has started!
 Your adversaries are:
-${Object
-    .values(players)
+${_.values(players)
     .filter(p => p.id !== lead.id)
     .map(p => `\n - ${p.first_name || 'Anonymous'}`)
     .join('')}
