dc78279fd3dda720dee4e82a33c8b057ca9acb41
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-21 08:11:45 -0700

    Removed the need for lodash.

--- src/lib/facebook/transformers/list.js
+++ src/lib/facebook/transformers/list.js
@@ -1,12 +1,10 @@
-import _ from 'lodash';
-
 export default (cards, style = 'compact') => ({
     attachment: {
         type: 'template',
         payload: {
             template_type: 'list',
             top_element_style: style,
-            elements: _.map(cards, ({ title, subtitle, imageUrl, buttons }) => ({
+            elements: cards.map(({ title, subtitle, imageUrl, buttons }) => ({
                 title,
                 subtitle: subtitle || null,
                 image_url: imageUrl || null,
