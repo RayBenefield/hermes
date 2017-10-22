8064631c083e856621aed88199093b63c0722d5b
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-22 04:07:11 -0700

    Added the card emoji for showing the selected card.

--- src/lib/facebook/transformers/card-already-selected-message.js
+++ src/lib/facebook/transformers/card-already-selected-message.js
@@ -4,7 +4,7 @@ import card from './card';
 export default ({ card: pick }) => [
     text({ text: 'You already picked:' }),
     card({
-        title: pick.contents,
+        title: `🃏 ${pick.contents}`,
         image: 'http://homepages.neiu.edu/~whuang2/cs300/images/white.png',
     }),
     text({ text: 'We\'ll let you know when your adversaries have selected their candidates.' }),
