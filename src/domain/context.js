7d0755650eee6e470c38246a379432d5643cc4e1
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-22 06:15:47 -0700

    Refactored out entity grabbing for context for readability.

--- src/domain/context.js
+++ src/domain/context.js
@@ -3,45 +3,46 @@ import uuid from 'uuid/v4';
 import transmute from 'transmutation';
 import whiteDeck from '../data/white-deck.json';
 
-export default ({ db }) => ({
-    get: {
-        login: transmute()
-            .extend('player', ({ lead }) =>
-                db.get(`players/${lead.platform}/${lead.id}`)),
-        join: transmute()
-            .extend('player', ({ lead }) =>
-                db.get(`players/${lead.platform}/${lead.id}`))
-            .extend('queue', () => db.get('queue')),
-        hand: transmute()
-            .extend('player', ({ lead }) =>
-                db.get(`players/${lead.platform}/${lead.id}`))
-            .extend('game', ({ payload: { game } }) =>
-                db.get(`games/${game}`))
-            .extend('round', ({ game, payload: { round } }) =>
-                db.get(`rounds/${game.id}/${round}`))
-            .extend('hand', ({ player, game }) =>
-                db.get(`hands/${game.id}/${player.id}`)),
-        pick: transmute()
-            .extend('player', ({ lead }) =>
-                db.get(`players/${lead.platform}/${lead.id}`))
-            .extend('game', ({ payload: { game } }) =>
-                db.get(`games/${game}`))
-            .extend('round', ({ game, payload: { round } }) =>
-                db.get(`rounds/${game.id}/${round}`))
-            .extend('whiteDeck', whiteDeck)
-            .extend('pick', ({ payload: { pick } }) => whiteDeck[pick]),
-    },
-    save: {
-        'welcome-message': transmute()
+export default ({ db }) => {
+    const entities = {
+        player: ['player', ({ lead }) => db.get(`players/${lead.platform}/${lead.id}`)],
+        queue: ['queue', () => db.get('queue')],
+        game: ['game', ({ payload: { game } }) => db.get(`games/${game}`)],
+        round: ['round', ({ game, payload: { round } }) => db.get(`rounds/${game.id}/${round}`)],
+        hand: ['hand', ({ player, game }) => db.get(`hands/${game.id}/${player.id}`)],
+        whiteDeck: ['whiteDeck', whiteDeck],
+        pick: ['pick', ({ payload: { pick } }) => whiteDeck[pick]],
+    };
+
+    return {
+        get: {
+            login: transmute().extend(...entities.player),
+            join: transmute()
+                .extend(...entities.player)
+                .extend(...entities.queue),
+            hand: transmute()
+                .extend(...entities.player)
+                .extend(...entities.game)
+                .extend(...entities.round)
+                .extend(...entities.hand),
+            pick: transmute()
+                .extend(...entities.player)
+                .extend(...entities.game)
+                .extend(...entities.round)
+                .extend(...entities.whiteDeck)
+                .extend(...entities.pick),
+        },
+        save: {
+            'welcome-message': transmute()
             .do(({ payload: lead }) =>
                 db.set(`players/${lead.platform}/${lead.id}`, lead)),
-        'welcome-back-message': transmute()
+            'welcome-back-message': transmute()
             .do(({ payload: { lead } }) =>
                 db.set(`players/${lead.platform}/${lead.id}`, lead)),
-        'queue-joined-message': transmute()
+            'queue-joined-message': transmute()
             .do(({ payload: { player } }) =>
                 db.set(`queue/${player.id}`, player)),
-        'game-started-message': transmute()
+            'game-started-message': transmute()
             .do(({ player, payload: { players } }) => {
                 const id = uuid();
                 db.set(`games/${id}`, {
@@ -51,10 +52,11 @@ export default ({ db }) => ({
                 });
                 db.delete(Object.values(players).map(p => `queue/${p.id}`));
             }),
-        'card-selected-message': transmute()
+            'card-selected-message': transmute()
             .do(({ payload: { game, round, card }, player }) =>
                 db.set(`rounds/${game.id}/${round.id}/candidates/${player.id}`, card))
             .do(({ payload: { game, card }, player }) =>
                 db.delete([`hands/${game.id}/${player.id}/${card.id}`], card)),
-    },
-});
+        },
+    };
+};
