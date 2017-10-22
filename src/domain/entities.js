5a4bb939f1f5043ba290114bee97952ace557f8c
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-22 10:26:56 -0700

    Modified the game started trigger to use the entities system.

--- src/domain/entities.js
+++ src/domain/entities.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines */
 import _ from 'lodash';
 import uuid from 'uuid/v4';
 import whiteDeck from '../data/white-deck.json';
@@ -19,6 +20,8 @@ export default ({ db }) => ({
         pick: ['pick', ({ payload: { pick } }) => whiteDeck[pick]],
         card: ['card', () => blackDeck.sort(() => 0.5 - Math.random()).slice(0, 1)[0]],
         players: ['players', ({ game: { players } }) => _.values(players)],
+        unnotifiedPlayers: ['unnotifiedPlayers', ({ players, game: { notified_players: notified } }) =>
+            players.filter(({ id }) => !(id in notified))],
     },
     save: {
         playerInfo: [({ payload: lead }) => db.set(`players/${lead.platform}/${lead.id}`, lead)],
@@ -38,5 +41,26 @@ export default ({ db }) => ({
             db.set(`rounds/${game.id}/${round.id}/candidates/${player.id}`, card)],
         removalOfCandidateFromHand: [({ payload: { game, card }, player }) =>
             db.delete([`hands/${game.id}/${player.id}/cards/${card.id}`])],
+        notifiedAllPlayers: [({ game: { id, players } }) =>
+            db.set(`games/${id}/notified_players`, players)],
+        gameForPlayers: [({ players, game: { id } }) => Promise.all(
+            players.map(player =>
+                db.set(`players/facebook/${player.id}/games/${id}`)))],
+        handsForPlayers: [({ players, game: { id } }) => Promise.all(players
+            .map(p => ({ player: p }))
+            .map(p => ({ ...p, hand: whiteDeck.sort(() => 0.5 - Math.random()).slice(0, 10) }))
+            .map(({ player, hand }) => ({
+                player,
+                hand: hand.reduce((h, c) => Object.assign(h, { [c.id]: c }), {}),
+            }))
+            .map(({ player, hand }) => db.set(`hands/${id}/${player.id}`, {
+                game: id,
+                player: player.id,
+                cards: hand,
+            })))],
+        newRound: [({ game }) => {
+            const id = uuid();
+            return db.set(`rounds/${game.id}/${id}`, { id, game: game.id });
+        }],
     },
 });
--- src/triggers/triggers/gameStarted.js
+++ src/triggers/triggers/gameStarted.js
@@ -1,40 +1,25 @@
-import _ from 'lodash';
-import uuid from 'uuid/v4';
 import transmute from 'transmutation';
-import whiteDeck from '../../data/white-deck.json';
+import configureEntites from '../../domain/entities';
 
-export default ({ db, fb }) => transmute()
-    .extend('messages', ({ game }) => [
-        {
-            type: 'game-started-message',
-            payload: game,
-        },
-    ])
-    .extend('leads', ({ game: { players, notified_players: notified } }) =>
-        _.values(players).filter(({ id }) => !(id in notified)))
-    .do(({ leads, messages }) => Promise.all(
-        leads.map(lead => transmute({ lead, messages })
-            .extend('facebookMessages', fb.transform)
-            .do(({ facebookMessages }) =>
-                fb.sendMessages(lead.id, facebookMessages)))))
-    .do(({ game: { id, players } }) =>
-        db.set(`games/${id}/notified_players`, players))
-    .do(({ game: { id, players } }) => Promise.all(_.values(players)
-        .map(player =>
-            db.set(`players/facebook/${player.id}/games/${id}`))))
-    .do(({ game: { id, players } }) => Promise.all(_.values(players)
-        .map(p => ({ player: p }))
-        .map(p => ({ ...p, hand: whiteDeck.sort(() => 0.5 - Math.random()).slice(0, 10) }))
-        .map(({ player, hand }) => ({
-            player,
-            hand: hand.reduce((h, c) => Object.assign(h, { [c.id]: c }), {}),
-        }))
-        .map(({ player, hand }) => db.set(`hands/${id}/${player.id}`, {
-            game: id,
-            player: player.id,
-            cards: hand,
-        }))))
-    .do(({ game }) => {
-        const id = uuid();
-        return db.set(`rounds/${game.id}/${id}`, { id, game: game.id });
-    });
+export default ({ db, fb }) => {
+    const { get, save } = configureEntites({ db });
+
+    return transmute()
+        .extend('messages', ({ game }) => [
+            {
+                type: 'game-started-message',
+                payload: game,
+            },
+        ])
+        .extend(...get.players)
+        .extend(...get.unnotifiedPlayers)
+        .do(({ unnotifiedPlayers, messages }) => Promise.all(
+            unnotifiedPlayers.map(lead => transmute({ lead, messages })
+                .extend('facebookMessages', fb.transform)
+                .do(({ facebookMessages }) =>
+                    fb.sendMessages(lead.id, facebookMessages)))))
+        .do(...save.notifiedAllPlayers)
+        .do(...save.gameForPlayers)
+        .do(...save.handsForPlayers)
+        .do(...save.newRound);
+};
