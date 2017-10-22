86ed721833a00e05162dd4a71a12fb10ea572ab0
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-22 15:14:04 -0700

    Removed already finished messages.

--- src/data/messages.js
+++ src/data/messages.js
@@ -1,38 +1,5 @@
 /* eslint-disable max-lines */
 export default {
-    start: () => [
-        {
-            type: 'new-goal-message',
-            payload: {
-                card: {
-                    contents: '________, the latest Facebook craze.',
-                },
-            },
-        },
-    ],
-    pick: () => [
-        {
-            type: 'card-selected-message',
-            payload: {
-                contents: 'Being a motherfucking sorcerer.',
-            },
-        },
-    ],
-    candidates: () => [
-        {
-            type: 'show-ranked-list',
-            payload: {
-                ranked: [
-                    { contents: 'Vigorous jazz hands' },
-                    { contents: 'Centaur Porn' },
-                ],
-                unranked: [
-                    { contents: 'Your Face' },
-                    { contents: 'Barack Obama' },
-                ],
-            },
-        },
-    ],
     vote: () => [
         {
             type: 'show-votes-message',
