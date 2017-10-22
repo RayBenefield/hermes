e7b16685e40018b82248bfa646f6c87ef35035ac
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-22 06:17:43 -0700

    Removed quick replies we do not need to test anymore.

--- src/data/actions.js
+++ src/data/actions.js
@@ -6,26 +6,6 @@ export default [
     },
     {
         content_type: 'text',
-        title: 'Join Queue',
-        payload: 'action: join',
-    },
-    {
-        content_type: 'text',
-        title: 'Join a Game',
-        payload: 'action: start',
-    },
-    {
-        content_type: 'text',
-        title: 'Show Hand',
-        payload: 'action: hand',
-    },
-    {
-        content_type: 'text',
-        title: 'Pick',
-        payload: 'action: pick',
-    },
-    {
-        content_type: 'text',
         title: 'Show Candidates',
         payload: 'action: candidates',
     },
