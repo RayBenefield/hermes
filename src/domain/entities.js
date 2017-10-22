e38cd889cf587bb71a5d122b670193e1fe8689d4
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-22 10:00:47 -0700

    Refactored all the context gathering for round start.

--- src/domain/entities.js
+++ src/domain/entities.js
@@ -1,5 +1,7 @@
+import _ from 'lodash';
 import uuid from 'uuid/v4';
 import whiteDeck from '../data/white-deck.json';
+import blackDeck from '../data/black-deck.json';
 
 export default ({ db }) => ({
     get: {
@@ -13,12 +15,15 @@ export default ({ db }) => ({
         round: ['round', ({ game, payload: { round } }) => db.get(`rounds/${game.id}/${round}`)],
         hand: ['hand', ({ player, game }) => db.get(`hands/${game.id}/${player.id}`)],
         whiteDeck: ['whiteDeck', whiteDeck],
+        blackDeck: ['blackDeck', blackDeck],
         pick: ['pick', ({ payload: { pick } }) => whiteDeck[pick]],
+        card: ['card', () => blackDeck.sort(() => 0.5 - Math.random()).slice(0, 1)[0]],
+        players: ['players', ({ game: { players } }) => _.values(players)],
     },
     save: {
         playerInfo: [({ payload: lead }) => db.set(`players/${lead.platform}/${lead.id}`, lead)],
         toQueue: [({ payload: { player } }) => db.set(`queue/${player.id}`, player)],
-        removalFromQueue: [({ payload: { players } }) => db.delete(Object.values(players).map(p => `queue/${p.id}`))],
+        removalFromQueue: [({ payload: { players } }) => db.delete(_.values(players).map(p => `queue/${p.id}`))],
         newGame: [({ player, payload: { players } }) => {
             const id = uuid();
             db.set(`games/${id}`, {
--- src/triggers/triggers/roundStarted.js
+++ src/triggers/triggers/roundStarted.js
@@ -1,25 +1,24 @@
-import _ from 'lodash';
 import transmute from 'transmutation';
 import configureEntities from '../../domain/entities';
-import blackDeck from '../../data/black-deck.json';
 
 export default ({ db, fb }) => {
     const { get } = configureEntities({ db });
 
     return transmute()
         .extend(...get.game)
-        .extend('card', () => blackDeck.sort(() => 0.5 - Math.random()).slice(0, 1)[0])
+        .extend(...get.blackDeck)
+        .extend(...get.card)
+        .extend(...get.players)
         .extend('messages', ({ round, game, card }) => [
             {
                 type: 'new-goal-message',
                 payload: { game, round, card },
             },
         ])
-        .extend('leads', ({ game: { players } }) => _.values(players))
         .do(({ game, round }) => db.push(`games/${game.id}/rounds`, round.id))
         .do(({ game, round, card }) => db.set(`rounds/${game.id}/${round.id}/card`, card))
-        .do(({ leads, messages }) => Promise.all(
-            leads.map(lead => transmute({ lead, messages })
+        .do(({ players, messages }) => Promise.all(
+            players.map(lead => transmute({ lead, messages })
                 .extend('facebookMessages', fb.transform)
                 .do(({ facebookMessages }) =>
                     fb.sendMessages(lead.id, facebookMessages)))));
