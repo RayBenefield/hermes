06e55da33e61f838e328da8532ed135b7575dbb4
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-28 08:33:18 -0700

    Properly add quick replies if in dev mode.

--- src/lib/facebook/transformers/text.js
+++ src/lib/facebook/transformers/text.js
@@ -1,6 +1,8 @@
 import actions from '../../../data/actions';
 
-export default ({ text }) => ({
-    text,
-    quick_replies: process.env.NODE_ENV === 'dev' ? actions : [],
-});
+export default ({ text }) => {
+    const message = { text };
+    if (process.env.NODE_ENV === 'dev') message.quick_replies = actions;
+
+    return message;
+};
