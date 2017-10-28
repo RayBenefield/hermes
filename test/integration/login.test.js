c58449a06e21d26e7015a7485be872b9dc17b9c0
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-28 10:11:17 -0700

    Initial version of snapshot integration testing.

---
+++ test/integration/login.test.js
@@ -0,0 +1,24 @@
+import flame from '@leonardvandriel/flame';
+import configureDomain from '../../functions/domain'; // eslint-disable-line import/no-extraneous-dependencies
+import configureFlame from '../../functions/lib/local-db';  // eslint-disable-line import/no-extraneous-dependencies
+
+const db = configureFlame(flame);
+const domain = configureDomain({ db });
+
+test('sends back a welcome message and instructions message', () => {
+    // Given
+    const lead = {
+        platform: 'facebook',
+        id: 123456,
+    };
+
+    // When
+    return domain({
+        lead,
+        action: 'login',
+    })
+        .then(results => expect({
+            results,
+            db: flame.get('/'),
+        }).toMatchSnapshot());
+});
