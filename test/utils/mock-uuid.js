ce347af2537950c857bc987d03fb036a27333baa
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-10-28 14:42:36 -0700

    Made sure that uuid constantly makes new ids.

--- test/utils/mock-uuid.js
+++ test/utils/mock-uuid.js
@@ -1,1 +1,7 @@
-export default () => 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
+let count = 0;
+export default () => {
+    const id = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
+        .replace(/[x]/g, String.fromCharCode(97 + count));
+    count++;
+    return id;
+};
